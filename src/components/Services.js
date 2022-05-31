679731784006006d35f8e18062ec1375aaa44f00
Ali <ali.zavari.cs@gmail.com>
2022-05-31 15:29:53 +0430

    initial-commit

---
+++ src/components/Services.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import styled from 'styled-components'
+import { services } from '../utils/constants'
+const Services = () => {
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <article className='header'>
+          <h3>
+            custom furniture <br /> built only for you
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            dolorum debitis consectetur reprehenderit non aliquam voluptates
+            dolore aut vero consequuntur.
+          </p>
+        </article>
+        <div className='services-center'>
+          {services.map((service) => {
+            const { id, icon, title, text } = service
+            return (
+              <article className='service' key={id}>
+                <span className='icon'>{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  h3,
+  h4 {
+    color: var(--clr-primary-1);
+  }
+  padding: 5rem 0;
+
+  background: var(--clr-primary-10);
+
+  .header h3 {
+    margin-bottom: 2rem;
+  }
+  p {
+    margin-bottom: 0;
+    line-height: 1.8;
+    color: var(--clr-primary-3);
+  }
+  .services-center {
+    margin-top: 4rem;
+    display: grid;
+    gap: 2.5rem;
+  }
+  .service {
+    transition: all 0.25s ease-in-out ;
+    background: var(--clr-primary-7);
+    text-align: center;
+    padding: 2.5rem 2rem;
+    border-radius: var(--radius);
+    p {
+      color: var(--clr-primary-2);
+    }
+  }
+  .service:hover{
+    box-shadow:0 5px 8px gray ;
+  }
+  span {
+
+    opacity: 0.7 ;
+    transition: all 0.15s ease-in-out ;
+    width: 4rem;
+    height: 4rem;
+    display: grid;
+    margin: 0 auto;
+    place-items: center;
+    margin-bottom: 1rem;
+    border-radius: 50%;
+    background: var(--clr-primary-10);
+    color: var(--clr-primary-1);
+    svg {
+      font-size: 2rem;
+    }
+  }
+  @keyframes animated{
+    0%{box-shadow:0 0 0 white}
+    50%{box-shadow:0 0 15px white}
+    100%{box-shadow: 0 0 0 white}
+  }
+  span:hover{
+    opacity:1 ;
+    animation: animated 1s;
+    animation-iteration-count:3 ;
+    animation-delay: 1s ;
+  }
+  @media (min-width: 992px) {
+    .header {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+  @media (min-width: 576px) {
+    .services-center {
+      grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
+    }
+  }
+  @media (min-width: 1280px) {
+    padding: 0;
+    .section-center {
+      transform: translateY(5rem);
+    }
+  }
+`
+export default Services
---
+++ src/utils/constants.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { GiCompass, GiDiamondHard, GiStabbedNote } from 'react-icons/gi'
+export const links = [
+  {
+    id: 1,
+    text: 'خانه',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'درباره ما',
+    url: '/about',
+  },
+  {
+    id: 3,
+    text: 'محصولات',
+    url: '/products',
+  },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: <GiCompass />,
+    title: 'اهداف ما',
+    text:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+  },
+  {
+    id: 2,
+    icon: <GiDiamondHard />,
+    title: 'ویژگی ما',
+    text:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+  },
+  {
+    id: 3,
+    icon: <GiStabbedNote />,
+    title: 'گذشته ما',
+    text:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+  },
+]
+
+export const products_url = 'https://course-api.com/react-store-products'
+
+export const single_product_url = `https://course-api.com/react-store-single-product?id=`
